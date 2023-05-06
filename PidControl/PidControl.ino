04af21c475bec7b7c9083fbd54411aa5b60c74a0
V16nesh <v16nesh2003@gmail.com>
2023-05-06 12:10:27 +0530

    added pid controller

--- PidControl/PidControl.ino
+++ PidControl/PidControl.ino
@@ -4,6 +4,15 @@
 #define m1dir 4
 
 int pos = 0;
+int p=0;
+int i=0;
+int d=0;
+
+
+
+int error=0;
+int preverror=0;
+
 
 
 
@@ -11,9 +20,9 @@ long prevT = 0;
 float eprev = 0;
 float eint = 0;
 
-float kp = 0.9;
-float ki = 0;
-float kd = 0.1;
+float kp = 10;
+float ki = 0.001;
+float kd = 2;
 
 
 void setup() {
@@ -27,27 +36,28 @@ void setup() {
 }
 
 void loop() {
-  int tar = 1000;
-  long curT = micros();
-  float delT = ((float)(curT-prevT))/1.0e6;
-  prevT = curT;
-
-  int e = pos-tar;
-
-  int edev = (e-eprev)/delT;
-
-  int eint = eint+(e*delT);
-
-  float u = kp*e+kd*edev+ki*eint;
-
-  ms(u);
-
-  eprev = e;
+  int tar = 600;
   
-  Serial.print(tar);
+  error = tar-pos;
+  p=error;
+  i=i+error;
+  d = error-preverror;
+
+  int pid = kp*p+kd*d+ki*i;
+    if(pid>255){
+    pid=255;
+    }
+  if(pid<-255){
+    pid=-255;
+  }
+  ms(pid);
+   Serial.print(tar);
   Serial.print(" ");
   Serial.print(pos);
   Serial.println();
+
+
+
 }
 
 void encoder(){
@@ -63,10 +73,10 @@ void encoder(){
 void ms(int s1){
   if(s1<0){
     s1 = -s1;
-    digitalWrite(m1dir, LOW);
+    digitalWrite(m1dir, HIGH);
     analogWrite(m1pwm, s1);
     }else{
-      digitalWrite(m1dir, HIGH);
+      digitalWrite(m1dir, LOW);
       analogWrite(m1pwm, s1);
       }
   }
